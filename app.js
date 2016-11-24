7b1c6dc3f9ca93dcb694e5983201ce06b2de85b3
Faisal Hassan <faisal.hassanx@gmail.com>
2016-11-24 19:31:44 +0000

    Finish project

--- app.js
+++ app.js
@@ -8,16 +8,6 @@ function getColor() {
     document.body.style.background = bgColor;
   }
 // runs function on click
- document.body.onkeyup = function(e){
-      if(e.keyCode == 13){
-        setBackground();
-      }
-  }
- {
-  var bgColor = getColor();
-  document.body.style.background = bgColor;
-}
-// runs function on click
 document.body.onkeyup = function(e){
   if(e.keyCode == 32) {
     setBackground();
